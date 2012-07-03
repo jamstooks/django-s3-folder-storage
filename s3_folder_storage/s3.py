6321a9aa0624e15fd00987fc174653f1026649ad
Ben <ben.stookey@gmail.com>
2012-07-03 16:33:16 -0400

    Initial Commit

---
+++ s3_folder_storage/s3.py
@@ -0,0 +1,26 @@
+"""
+    Two classes for media storage
+"""
+
+from storages.backends.s3boto import S3BotoStorage
+from django.conf import settings
+
+class StaticStorage(S3BotoStorage):
+    """
+        Storage for static files.
+        The folder is defined in settings.STATIC_S3_PATH
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = settings.STATIC_S3_PATH
+        super(StaticStorage, self).__init__(*args, **kwargs)
+
+class DefaultStorage(S3BotoStorage):
+        """
+            Storage for uploaded media files.
+            The folder is defined in settings.DEFAULT_S3_PATH
+        """
+
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = settings.DEFAULT_S3_PATH
+        super(MediaStorage, self).__init__(*args, **kwargs)
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+# Utility function to read README file
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='django-s3-folder-storage',
+      version='0.1',
+      description="Quick extension of django-storages' S3BotoStorage to allow separate media and static folders within a bucket.",
+      author='Benjamin W Stookey',
+      author_email='ben.stookey@gmail.com',
+      url='https://github.com/jamstooks/django-s3-folder-storage',
+      long_description=read("README.md"),
+      packages=['django-s3-folder-storage'],
+      install_requires=['django-storages'],
+      )
